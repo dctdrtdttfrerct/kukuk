043ec66f2e72a028b0d9ae8916ff661ad99f2fdc
dctdrtdttfrerct <erykoliwa7@gmail.com>
2026-09-19 08:51:55 +0200

    Add Firebase configuration and initialization

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// Konfiguracja Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAE0z2_4ylnhao9x9FZAXGQVqqCZPXUBNo",
+  authDomain: "kuku-informacje.firebaseapp.com",
+  projectId: "kuku-informacje",
+  storageBucket: "kuku-informacje.appspot.com",
+  messagingSenderId: "814044021410",
+  appId: "1:814044021410:web:9cbe8772d011f2dbcd4dfb",
+  measurementId: "G-HBY0JML1QL"
+};
+
+// Inicjalizacja
+firebase.initializeApp(firebaseConfig);
+
+const storage = firebase.storage();
+const db = firebase.firestore();
